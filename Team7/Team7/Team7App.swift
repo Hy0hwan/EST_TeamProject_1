889d7755de905278d82821543ffadc8d531803b0
김효환 <gyghks1017@gmail.com>
2025-05-09 17:50:42 +0900

    feat : 프로젝트 초기 셋팅

---
+++ Team7/Team7/Team7App.swift
@@ -0,0 +1,17 @@
+//
+//  Team7App.swift
+//  Team7
+//
+//  Created by 김효환 on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Team7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
