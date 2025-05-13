9da6b01fe65e5d712f59485cc54fe68eccfb0989
kimgyeongun <kimgyeongun5@gmail.com>
2025-05-14 00:08:58 +0900

    fix: remove Desktop folder from Git tracking

---
+++ Team7/Team7/Views/WordForm/CreateView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateView.swift
+//  Team7
+//
+//  Created by 김경언 on 5/13/25.
+//
+
+import SwiftUI
+
+struct CreateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateView()
+}
