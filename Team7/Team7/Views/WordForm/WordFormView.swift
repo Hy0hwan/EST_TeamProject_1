3d1984486eac7950eca97c22f52e8aca5639afa7
kimgyeongun <kimgyeongun5@gmail.com>
2025-05-13 11:30:57 +0900

    WIP: 단어 작성 화면 레이아웃 구성 중

--- Team7/Team7/Views/WordForm/WordFormView.swift
+++ Team7/Team7/Views/WordForm/WordFormView.swift
@@ -6,72 +6,82 @@
 //
 
 import SwiftUI
+import SwiftData
 // // Create/Update 화면 (기본 입력 UI만 포함)
 
 struct WordFormView: View {
+    @Environment(\.modelContext) private var context
     @State private var protocolword: String = ""
     @State private var tag: String = ""
     @State private var meaning: String = ""
     
+    @State private var navigateToDetail = false
+    @State private var savedWord: Word? = nil
+    
     var body: some View {
-        VStack(spacing: 40) {
-            
-            
-            HStack {
-                Button("취소") {
+        NavigationStack {
+            VStack(spacing: 40) {
+                HStack {
+                    Button("취소") {
+                    }
+                    
+                    Spacer()
+                    
+                    Button("저장") {
+                        let newWord = Word(
+                            wordName: protocolword,
+                            wordDefinition: meaning,
+                            tag: Tag(name: tag)
+                            )
+                        context.insert(newWord)
+                        
+                        
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.top)
+                
+                
+                Text("단어 작성")
+                    .font(.title2)
+                    .bold()
+                    .frame(height: 20)
+                
+                VStack(spacing:20) {
+                    TextField("Protocol", text: $protocolword)
+                        .padding(.horizontal, 12)
+                        .frame(height: 45)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.gray.opacity(0.5), lineWidth: 1))
+                    
+                    
+                    TextField("태그", text: $tag)
+                        .padding(.horizontal, 12)
+                        .frame(height: 45)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.gray.opacity(0.5), lineWidth: 1))
+                    
+                    
+                    TextEditor(text: $meaning)
+                        .frame(height: 200)
+                        .padding(.top, 8) // 텍스트를 위로 밀어올림
+                        .padding(.horizontal, 4)
+                        .background(Color.white)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.gray.opacity(0.5), lineWidth: 1))
                     
                 }
+                .padding(.horizontal)
                 
                 Spacer()
                 
-                
-                Button("저장") {
-                }
             }
-            .padding(.horizontal)
-            .padding(.top)
-            
-            
-            Text("단어 작성")
-                .font(.title2)
-                .bold()
-                .frame(height: 20)
-            
-            VStack(spacing:20) {
-                TextField("Protocol", text: $protocolword)
-                    .padding(.horizontal, 12)
-                    .frame(height: 45)
-                    .background(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.gray.opacity(0.5), lineWidth: 1))
-                
-                
-                TextField("태그", text: $tag)
-                    .padding(.horizontal, 12)
-                    .frame(height: 45)
-                    .background(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.gray.opacity(0.5), lineWidth: 1))
-                
-                
-                TextEditor(text: $meaning)
-                    .frame(height: 200)
-                    .padding(.top, 8) // 텍스트를 위로 밀어올림
-                    .padding(.horizontal, 4)
-                    .background(Color.white)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.gray.opacity(0.5), lineWidth: 1))
-                
-            }
-            .padding(.horizontal)
-            
-            Spacer()
-            
         }
     }
-}
-
-#Preview {
-    WordFormView()
-}
+    
+    #Preview {
+        WordFormView()
+    }
