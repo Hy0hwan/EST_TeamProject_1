9b529e2259e0eece0aea3acecbb7b890a58ee573
서희재 <kitcat92.seo@gmail.com>
2025-05-13 13:55:16 +0900

    refactor: 태그 관련 뷰 구조 변경, 단어 모델 앤티티 타입 변경

--- Team7/Team7/Model/Word.swift
+++ Team7/Team7/Model/Word.swift
@@ -13,13 +13,13 @@ import SwiftData
 final class Word {
     var wordName: String // 단어 이름
     var wordDefinition: String // 단어 뜻
-    var tag: Tag? // 태그, 옵셔널 항목이므로 사용 시에는 unwrapping 필요
+    var tag: String? // 태그, 옵셔널 항목이므로 사용 시에는 unwrapping 필요
     var createdAt: Date // 생성일
     
     init(
         wordName: String,
         wordDefinition: String,
-        tag: Tag?,
+        tag: String?,
         createdAt: Date = .now // 데이터 생성 시에 생성일이 자동으로 입력
     ) {
         self.wordName = wordName
