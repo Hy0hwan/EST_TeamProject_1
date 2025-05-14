fd49a180735f542adc196f4c867c4ca8699869f1
김효환 <gyghks1017@gmail.com>
2025-05-14 11:43:47 +0900

    refactor : 모델컨테이너 호출방법 변경

--- Team7/Team7/Team7App.swift
+++ Team7/Team7/Team7App.swift
@@ -10,26 +10,25 @@ import SwiftData
 
 @main
 struct Team7App: App {
-// ModelContainer 에러 처리를 위해 작성했으나 프리뷰에서 자꾸 오류가 나서 주석 처리
-//    let container: ModelContainer
-//
-//    init() {
-//        do {
-//            container = try ModelContainer(for: Tag.self, Word.self)
-//        } catch {
-//            print("ModelContainer initialization failed with error: \(error)")
-//            fatalError("Failed to create ModelContainer: \(error)")
-//        }
-//    }
-//    
+    let container: ModelContainer
+
+    init() {
+        do {
+            container = try ModelContainer(for: Tag.self, Word.self)
+            print("성공 :  \(container)")
+        } catch {
+//            print("실패 :  \(ModelContainer(for: Tag.self, Word.self))")
+            fatalError("실패 : \(error)")
+
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             AppView()
         }
-        .modelContainer(
-            try! ModelContainer(for: Tag.self, Word.self) // 기존과 같이 강제 초기화
-        )
-//        .modelContainer(container)
+        .modelContainer(container)
     }
 }
+
 
