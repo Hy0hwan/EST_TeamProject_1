38f6a84483c1a82e42626aec84e5973d2391da92
김효환 <gyghks1017@gmail.com>
2025-05-15 15:16:01 +0900

    chore : SwiftData Word의 Tag 참조값 String -> Tag(SwiftData) 로 변경

--- Team7/Team7/Model/Word.swift
+++ Team7/Team7/Model/Word.swift
@@ -12,13 +12,13 @@ import SwiftData
 final class Word {
     var wordName: String // 단어 이름
     var wordDefinition: String // 단어 뜻
-    var tag: String? // 태그, 옵셔널 항목이므로 사용 시에는 unwrapping 필요, Tag.name 을 String 으로 전달 받아 사용하기 위해 단순하게 String? 타입으로 지정
+    var tag: Tag? // 태그, 옵셔널 항목이므로 사용 시에는 unwrapping 필요, Tag.name 을 String 으로 전달 받아 사용하기 위해 단순하게 String? 타입으로 지정 -> 효환 : SwiftData Tag를 참조
     var createdAt: Date // 생성일
     
     init(
         wordName: String,
         wordDefinition: String,
-        tag: String?,
+        tag: Tag?,
         createdAt: Date = .now // 데이터 생성 시에 생성일이 자동으로 입력
     ) {
         self.wordName = wordName
